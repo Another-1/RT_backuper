ec1965e768734328233455b80a695b70eadd4c69
Dmitry P <9990899+Another-1@users.noreply.github.com>
2022-09-04 21:11:01 +0300

    Update RT_collector.ps1

--- RT_collector.ps1
+++ RT_collector.ps1
@@ -24,6 +24,11 @@ if ( $torrents_list.count -eq 0) {
 Write-Output 'Ставим раздачи на закачку'
 ForEach ( $id in $torrents_list.Keys ) {
     $hash = (( Invoke-WebRequest -Uri ( 'http://api.rutracker.org/v1/get_tor_hash?by=topic_id&val=' + $id ) ).content | ConvertFrom-Json -AsHashtable ).result[$id]
-    $reqdata = 'urls=magnet:?xt=urn:btih:' + $hash + '&tr=http://bt.t-ru.org/ann?magnet&tr=http://bt2.t-ru.org/ann?magnet&tr=http://bt3.t-ru.org/ann?magnet&tr=http://bt4.t-ru.org/ann?magnet'
-    Invoke-WebRequest  -Uri ( $client_url + '/api/v2/torrents/add' ) -Body $reqdata -WebSession $sid -Method Post > nul
+    # $reqdata = 'urls=magnet:?xt=urn:btih:' + $hash + '&tr=http://bt.t-ru.org/ann?magnet&tr=http://bt2.t-ru.org/ann?magnet&tr=http://bt3.t-ru.org/ann?magnet&tr=http://bt4.t-ru.org/ann?magnet'
+    $reqdata = 'urls=magnet:?xt=urn:btih:' + $hash
+    Invoke-WebRequest  -Uri ( $client_url + '/api/v2/torrents/add' ) -Body $reqdata -WebSession $sid -Method Post > $nul
+    Start-Sleep -Seconds 2
+    $reqdata = 'hash=' + $hash + '&urls=http%3A%2F%2Fbt.t-ru.org%2Fann%3Fmagnet%0Ahttp%3A%2F%2Fbt2.t-ru.org%2Fann%3Fmagnet%0Ahttp%3A%2F%2Fbt3.t-ru.org%2Fann%3Fmagnet%0Ahttp%3A%2F%2Fbt4.t-ru.org%2Fann%3Fmagnet'
+    Invoke-WebRequest  -Uri ( $client_url + '/api/v2/torrents/addTrackers' ) -Body $reqdata -WebSession $sid -Method Post > $nul
+
 }

ec4cc04c89af116b09061ced08bda5c309d1511e
Dmitry P <9990899+Another-1@users.noreply.github.com>
2022-09-04 18:50:10 +0300

    Update RT_collector.ps1

--- RT_collector.ps1
+++ RT_collector.ps1
@@ -2,8 +2,8 @@
 $client_url = 'http://192.168.0.232:8082'
 
 # учётные данные для WebUI qBittorrent.
-$webui_login = 'admin'
-$webui_password = 'mainstreet'
+$webui_login = 'login'
+$webui_password = 'password'
 
 $choice = ( Read-Host -Prompt 'Выберите раздел' ).ToString()
 

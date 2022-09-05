339edb4327c35a21e06343daf951cf0ffa5031dc
Dmitry P <9990899+Another-1@users.noreply.github.com>
2022-09-06 00:17:56 +0300

    Add files via upload

---
+++ RT_settings.ps1
@@ -0,0 +1,9 @@
+# ссылка на WebUI qBittorrent.
+$client_url = 'http://192.168.0.232:8082'
+
+# учётные данные для WebUI qBittorrent.
+$webui_login = 'admin'
+$webui_password = 'mainstreet'
+
+# тут указываем каталог общих папок Google Drive.
+$google_folder = 'M:\Shared drives'

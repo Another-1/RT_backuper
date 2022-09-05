79addb8eac4f10ba31ba9d58a2abb4aef1e0d248
Dmitry P <9990899+Another-1@users.noreply.github.com>
2022-09-06 00:18:45 +0300

    Update RT_settings.ps1

--- RT_settings.ps1
+++ RT_settings.ps1
@@ -2,8 +2,8 @@
 $client_url = 'http://192.168.0.232:8082'
 
 # учётные данные для WebUI qBittorrent.
-$webui_login = 'admin'
-$webui_password = 'mainstreet'
+$webui_login = 'webui_login'
+$webui_password = 'webui_password'
 
 # тут указываем каталог общих папок Google Drive.
 $google_folder = 'M:\Shared drives'

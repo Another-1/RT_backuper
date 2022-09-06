932c9e09715960c916185a9a8eb0b8076b8e1294
Dmitry P <9990899+Another-1@users.noreply.github.com>
2022-09-06 11:06:06 +0300

    Update RT_settings.ps1

--- RT_settings.ps1
+++ RT_settings.ps1
@@ -7,3 +7,32 @@ $webui_password = 'webui_password'
 
 # тут указываем каталог общих папок Google Drive.
 $google_folder = 'M:\Shared drives'
+
+# учётные данные форума
+$rutracker_login = 'forum_login'
+$rutracker_password = 'forum_password'
+
+# адрес и пароль прокси-сервера
+$proxy_address = 'http://45.8.144.130:3128'
+$proxy_password = 'proxy_password'
+
+# осздавать ли подкаталоги по ID для раздачи при восстановлении из архивов. 1 - создавать, 0 - не надо
+$torrent_folders = 0
+
+# каталог временных файлов для скачивания .torrent при восстановлении
+$temp_folder = 'C:\TEMP'
+
+# каталог для распаковки добавляемых раздач при восстановлении
+$store_path = 'E:\Хранимое\Прочее'
+
+# ссылка на архиватор 7z
+$7z_path = 'c:\Program Files\7-Zip\7z.exe'
+
+# пароль на распаковку архивов. Следует оставить как есть если не требуется иное в явном виде.
+$archive_password = '20RuTracker.ORG22'
+
+# максимальное количество сидов для подхвата
+$max_seeders = 3
+
+# категория для скачиваемых раздач. Если не указана, берётся с трекера
+$default_category = 'Временное'

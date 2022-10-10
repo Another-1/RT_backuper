e7f839ebb5474b0a6952ee90c57b04814314e74b
Evgen <leo.jeka.work@gmail.com>
2022-10-10 20:42:06 +0300

    update docker & run

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,21 @@
 FROM mcr.microsoft.com/powershell
 
 RUN apt-get update && \
-    apt-get install -y --no-install-recommends p7zip-full tzdata && \
+    apt-get install -y --no-install-recommends p7zip-full tzdata locales && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
+# Locale
+ENV TZ Europe/Moscow
+ENV LANG ru_RU.UTF-8
+ENV LANGUAGE ru_RU:ru
+ENV LC_LANG ru_RU.UTF-8
+ENV LC_ALL ru_RU.UTF-8
+
+RUN sed -i -e 's/# ru_RU.UTF-8 UTF-8/ru_RU.UTF-8 UTF-8/' /etc/locale.gen && locale-gen && \
+    ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
 WORKDIR /pwd
 COPY . .
 
-VOLUME ["/pwd/config"]
-CMD ["pwsh", "/pwd/RT_run.ps1"]
+VOLUME ["/pwd/config", "/pwd/stash"]
+ENTRYPOINT ["pwsh", "/pwd/RT_run.ps1"]
--- RT_run.ps1
+++ RT_run.ps1
@@ -1,30 +1,36 @@
-# Задаем файл из которых будем читать список раздач, которые надо обновить сейчас
-$hashes_file = "$PSScriptRoot/config/hashes.txt"
-
-$runs = 1
-while( $runs -le 9999 ) {
-    $uselles_run = $true
-    if ( Test-Path -path $hashes_file ) {
-        # Читаем список хешей, которые следует обработать
-        $hashes = Get-Content $hashes_file | Get-Unique
-        # Очищаем файл
-        Clear-Content -Path $hashes_file
-    }
-
-    if ( $hashes.count -gt 0 ) {
-        Write-Host ( 'Найдено хешей для обработки: ' + $hashes.count )
-        foreach ($hash in $hashes) {
-            Write-Host ( $hash )
-            .$PSScriptRoot/RT_backuper_New.ps1 $hash
+# Зацикливаем файл. Если передано название файла, то вызываем его.
+# name_param timer
+#   RT_{name} - название файла, который будет вызван
+#   param     - параметр, который будет передан в вызываемый файл
+#   timer     - переопределение паузы между итерациями цикла
+$run = @{
+    num = 0
+    timer = 60
+    start = $null
+    exec_time = 0
+}
+while( $true ) {
+    $run.start = Get-Date
+    if ( $args.count -gt 0 ) {
+        if ( $args[1] -ne $null ) {
+            try { $run.timer = [int]$args[1] } catch {}
         }
-        $uselles_run = $false
+
+        $proc, $param = $args[0].trim('_').Split('_')
+        $run_file = "$PSScriptRoot/RT_{0}.ps1" -f $proc
+        Write-Output ( '[{0}] Params: [{1}], file: {2}' -f (Get-Date -Format t), ($args -Join ','), $run_file )
+        if ( Test-Path $run_file ) {
+            if ( $param ) {
+                .$run_file $param
+            } else {
+                .$run_file
+            }
+        }
     }
-    else {
-        .$PSScriptRoot/RT_backuper_New.ps1
-    }
-    if ( $uselles_run ) {
-        Write-Output "Подождём часик и попробуем ещё раз. Счётчик цикла: $runs"
-        Start-Sleep -Seconds (60 * 60)
-    }
-    $runs++
+
+    $run.exec_time = [math]::Round( ((Get-Date) - $run.start).TotalMinutes )
+    $run.timer = if ( $run.exec_time -ge $run.timer ) { 1 } else { $run.timer - $run.exec_time }
+    $run.num++
+    Write-Output ( '[{0}] Подождём {1} минут и попробуем ещё раз. Счётчик цикла: {2}' -f (Get-Date -Format t), $run.timer, $run.num )
+    Start-Sleep -Seconds ($run.timer * 60)
 }

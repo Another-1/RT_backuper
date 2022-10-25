84897c9cd2fc7424d9f595cff6a4d5f7c1db89fb
Evgen <leo.jeka.work@gmail.com>
2022-10-25 20:10:30 +0300

    example

--- RT_functions.ps1
+++ RT_functions.ps1
@@ -23,10 +23,10 @@ $stash_folder = @{
     default       = "$PSScriptRoot/stash"                   # Общий путь к папке
     archived      = "$PSScriptRoot/stash/archived"          # Путь к спискам архивов по дискам
     uploads_limit = "$PSScriptRoot/stash/uploads_limit.xml" # Файл записанных отдач (лимиты)
-    backup_list   = "$PSScriptRoot/stash/backup_{0}.xml"    # Файл уже обработанного списка раздач. Для случая когда был перезапуск
-
-    finished_list = "$PSScriptRoot/stash/finished_{0}.txt"      # Файл id_hash раздач, которые были найдены в гугле или были заархивированны
     pause         = "$PSScriptRoot/stash/pause.txt"         # Если в файле что-то есть, скрипт встанет на паузу.
+
+    backup_list   = "$PSScriptRoot/stash/{0}/backup_list.xml"    # Файл уже обработанного списка раздач. Для случая когда был перезапуск
+    finished_list = "$PSScriptRoot/stash/{0}/finished_list.txt"  # Файл id_hash обработанных раздач, которые надо уделать, если это необходимо
 }
 
 $def_paths = @{

72552e3e6b23f32b14fa3fae0edbb9b5a5595d05
Another-1 <itsmyownaddress@gmail.com>
2022-11-27 13:33:06 +0300

    Add RT_mover fist look

---
+++ RT_mover.ps1
@@ -0,0 +1,48 @@
+param (
+    [Alias("src")] [string]$disk_from,
+    [ArgumentCompleter({ param($cmd, $param, $word) [array]( Get-Content "$PSScriptRoot/clients.txt" ) })]
+    [Alias("scl")][string]$client_from = 'NAS-2',
+    [ArgumentCompleter({ param($cmd, $param, $word) [array]( Get-Content "$PSScriptRoot/clients.txt" ) })]
+    [Alias("dcl")][string]$client_to = 'NAS'
+)
+
+. "$PSScriptRoot\RT_functions.ps1"
+
+if ( !(Confirm-Version) ) { Exit }
+if ( !( Sync-Settings ) ) { Pause; Exit }
+
+$client = Select-Client $client_from
+Initialize-Client
+
+$torrents_list = Get-ClientTorrents | Where-Object { $_.content_path -match '^\\' } | ForEach-Object {
+    foreach ( $key in $mover.keys ) {
+        if ( $_.content_path -match $key ) { $_ ; break }
+    }
+}
+$torrents_list = Get-TopicIDs $torrents_list $null
+
+$client = Select-Client $client_to
+Initialize-Client
+Initialize-Forum
+$torrents_list | ForEach-Object {
+    $torrent_file = Get-ForumTorrentFile $_.topic_id
+    $move_path = $_.save_path
+    foreach ( $key in $mover.keys ) {
+        if ( $move_path -match $key ) { $move_path = $move_path -replace ( ".*$key", $mover[$key] ); break }
+    }
+    Write-Host ( '[mover] Подхватываем ' + $_.name + ' в ' + $move_path )
+    $client = Select-Client $client_to
+    Add-ClientTorrent $_.hash $torrent_file.FullName $move_path $_.Category > $null
+    Remove-Item $torrent_file.FullName
+    try {
+        Write-Host ( '[delete] Удаляем из клиента раздачу {0}' -f $torrent_id )
+        $client = Select-Client $client_from
+        $request_delete = @{ hashes = $_.hash; deleteFiles = $false }
+        Read-Client 'torrents/delete' $request_delete > $null
+    }
+    catch {
+        Write-Host ( '[delete] Почему-то не получилось удалить раздачу {0}.' -f $torrent_id )
+    }
+
+    Start-Sleep -Seconds 1
+}
